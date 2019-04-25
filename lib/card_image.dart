f24fa96eeb1c27e0fc46b17ac5b2dde97e20dd30
nandyme <fernandomellone2@gmail.com>
2019-04-25 18:07:14 -0400

    started stateful but I need to go home now xD

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'floating_action_button_green.dart';
 
 
 
@@ -39,7 +39,13 @@ class CardImage extends StatelessWidget {
     );
 
 
-    return card;
+    return Stack(
+      alignment: Alignment(0.9, 1.1),
+      children: <Widget>[
+        card,
+        FloatingActionButtonGreen()
+      ],
+    );
   }
 
 }
--- lib/floating_action_button_green.dart
+++ lib/floating_action_button_green.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
 class FloatingActionButtonGreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FloatingActionButtonGreen();
+  }
 
 }
 
-
 class _FloatingActionButtonGreen extends State<FloatingActionButton> {
+
+  void onPressedFav() {}
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return FloatingActionButton(
       backgroundColor: Color(0xFF11DA53),
-
+      mini: true,
+      tooltip: "Fav",
+      onPressed: onPressedFav,
+      child: Icon(Icons.favorite_border),
     );
   }
+
 }
