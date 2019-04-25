893c08d5281b0ae7e243a0fad1df2f02c067d545
nandyme <fernandomellone2@gmail.com>
2019-04-25 17:35:58 -0400

    started stateful but I need to go home now xD

---
+++ lib/floating_action_button_green.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonGreen extends StatefulWidget {
+
+}
+
+
+class _FloatingActionButtonGreen extends State<FloatingActionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Color(0xFF11DA53),
+
+    );
+  }
+}
